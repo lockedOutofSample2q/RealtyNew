7770af0500cddafbd34d73cac096360fe9d40e78
lockedOutOfSample2q <hargunrajs@gmail.com>
2026-05-02 12:54:39 +0530

    feat: add RealEstateAgent and Person schema to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,62 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://*.supabase.co" />
         <link rel="dns-prefetch" href="https://*.supabase.co" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@graph": [
+                {
+                  "@type": "RealEstateAgent",
+                  "@id": "https://www.realtyconsultants.in/#organization",
+                  "name": "Realty Holding and Management Consultants",
+                  "url": "https://www.realtyconsultants.in/",
+                  "telephone": "+91-7814613916",
+                  "logo": "https://www.realtyconsultants.in/assets/logo-dark-theme.svg",
+                  "image": "https://www.realtyconsultants.in/assets/logo-dark-theme.svg",
+                  "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "Phase 8A, E328, Industrial Area, Sector 75",
+                    "addressLocality": "Sahibzada Ajit Singh Nagar",
+                    "addressRegion": "Punjab",
+                    "postalCode": "160055",
+                    "addressCountry": "IN"
+                  },
+                  "geo": {
+                    "@type": "GeoCoordinates",
+                    "latitude": "30.7028", 
+                    "longitude": "76.7111"
+                  },
+                  "areaServed": [
+                    "Mohali", "Banur", "Tepla Road", "Aerocity", "Aerotropolis", "New Chandigarh", "IT City"
+                  ],
+                  "sameAs": [
+                    "https://www.youtube.com/@AmritRealty",
+                    "https://www.facebook.com/people/AmritRealty/61587441234909/",
+                    "https://www.linkedin.com/company/reality-holding-and-management-consultants",
+                    "https://maps.app.goo.gl/H3Bxgv5v6fx3k3mEA",
+                    "https://wa.me/917814613916"
+                  ],
+                  "member": {
+                    "@id": "https://www.realtyconsultants.in/#person"
+                  }
+                },
+                {
+                  "@type": "Person",
+                  "@id": "https://www.realtyconsultants.in/#person",
+                  "name": "Amritpal Singh",
+                  "jobTitle": "Founder & Principal Consultant",
+                  "worksFor": { "@id": "https://www.realtyconsultants.in/#organization" },
+                  "sameAs": [
+                    "https://www.linkedin.com/in/amritpal-singh-29b14795/",
+                    "https://www.instagram.com/amritrealty"
+                  ]
+                }
+              ]
+            })
+          }}
+        />
       </head>
       <body className={`${cormorant.variable} ${dmSans.variable} ${dmMono.variable} overflow-x-hidden font-body`}>
         {children}
