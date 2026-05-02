625e0fb79a835b112161c2069a2963eaced93d0f
lockedOutOfSample2q <hargunrajs@gmail.com>
2026-05-02 17:31:05 +0530

    feat: switch to high-reliability OpenMap themes (Voyager & Detailed)

--- components/ui/PropertiesMap.tsx
+++ components/ui/PropertiesMap.tsx
@@ -81,12 +81,12 @@ export default function PropertiesMap({
         {mapTheme === "light" ? (
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           />
         ) : (
           <TileLayer
-            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
           />
         )}
         <ZoomControl position="topleft" />
