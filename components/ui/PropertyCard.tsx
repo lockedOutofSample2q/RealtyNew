42a2e476d9198e571ef6da7118f05a9df4d5f887
lockedOutOfSample2q <hargunrajs@gmail.com>
2026-06-13 14:39:22 +0530

    Fix: Add missing React import in PropertyCard.tsx

--- components/ui/PropertyCard.tsx
+++ components/ui/PropertyCard.tsx
@@ -1,5 +1,6 @@
 // components/ui/PropertyCard.tsx
 "use client";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
